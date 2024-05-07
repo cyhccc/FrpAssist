dfae60bd935cf15e15accd8caef7ca1ad5dcd649
cyhccc <caiyuhang_work@163.com>
2024-05-07 14:11:42 +0800

    123

---
+++ assistfrpc/build/release/moc_custombutton.cpp
@@ -0,0 +1,203 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'custombutton.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../component/custombutton.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'custombutton.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.14.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_CustomButton_t {
+    QByteArrayData data[8];
+    char stringdata0[58];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_CustomButton_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_CustomButton_t qt_meta_stringdata_CustomButton = {
+    {
+QT_MOC_LITERAL(0, 0, 12), // "CustomButton"
+QT_MOC_LITERAL(1, 13, 7), // "clicked"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 10), // "clicked_id"
+QT_MOC_LITERAL(4, 33, 2), // "id"
+QT_MOC_LITERAL(5, 36, 7), // "BtnType"
+QT_MOC_LITERAL(6, 44, 7), // "general"
+QT_MOC_LITERAL(7, 52, 5) // "color"
+
+    },
+    "CustomButton\0clicked\0\0clicked_id\0id\0"
+    "BtnType\0general\0color"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_CustomButton[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       1,   28, // properties
+       1,   31, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x06 /* Public */,
+       3,    1,   25,    2, 0x06 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::SChar,    4,
+
+ // properties: name, type, flags
+       4, QMetaType::SChar, 0x00095103,
+
+ // enums: name, alias, flags, count, data
+       5,    5, 0x0,    2,   36,
+
+ // enum data: key, value
+       6, uint(CustomButton::general),
+       7, uint(CustomButton::color),
+
+       0        // eod
+};
+
+void CustomButton::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<CustomButton *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->clicked(); break;
+        case 1: _t->clicked_id((*reinterpret_cast< qint8(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (CustomButton::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&CustomButton::clicked)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (CustomButton::*)(qint8 );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&CustomButton::clicked_id)) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty) {
+        auto *_t = static_cast<CustomButton *>(_o);
+        Q_UNUSED(_t)
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< qint8*>(_v) = _t->getId(); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::WriteProperty) {
+        auto *_t = static_cast<CustomButton *>(_o);
+        Q_UNUSED(_t)
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: _t->setId(*reinterpret_cast< qint8*>(_v)); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::ResetProperty) {
+    }
+#endif // QT_NO_PROPERTIES
+}
+
+QT_INIT_METAOBJECT const QMetaObject CustomButton::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_CustomButton.data,
+    qt_meta_data_CustomButton,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *CustomButton::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *CustomButton::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CustomButton.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int CustomButton::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty || _c == QMetaObject::WriteProperty
+            || _c == QMetaObject::ResetProperty || _c == QMetaObject::RegisterPropertyMetaType) {
+        qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyDesignable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyScriptable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyStored) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyEditable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyUser) {
+        _id -= 1;
+    }
+#endif // QT_NO_PROPERTIES
+    return _id;
+}
+
+// SIGNAL 0
+void CustomButton::clicked()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void CustomButton::clicked_id(qint8 _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
